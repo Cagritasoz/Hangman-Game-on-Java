0e41026c2ee29f8d1308da4b380d7ac213eea73c
Cagritasoz <cagritasoz@gmail.com>
2025-03-31 16:50:54 +0300

    Coded from scratch since last attempt got too complex.

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,182 +1,113 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 
-class DuplicateItemException extends RuntimeException {
-    public DuplicateItemException(String message) {
+class EmptySetOfWordsException extends RuntimeException {
+    public EmptySetOfWordsException(String message) {
         super(message);
     }
 }
 
-class GameMechanic {
-    private List<String> words;
-    private List<String> guessedLetters;
-    private final String filePath = "C:/Users/VICTUS/Desktop/Code/JAVA/Test/words.txt";
+class WordsSet {
+    private Set<String> words;
+    private final static String filePath = "C:/Users/VICTUS/Desktop/Code/JAVA/Test/words.txt";
 
-    public GameMechanic() {
-        this.words = new ArrayList<>();
-        this.guessedLetters = new ArrayList<>();
+    public WordsSet() throws EmptySetOfWordsException {
+        this.words = loadWords();
+        if (words.isEmpty()) {
+            throw new EmptySetOfWordsException("Word Set is empty! You need to add some words to the Word Set!");
+        }
     }
 
-    public List<String> getCurrentList() { //To get the current List
-        List<String> currentList = new ArrayList<>();
+    public Set<String> loadWords() {
+        Set<String> wordsSet = new HashSet<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(filePath));
             String line;
             while ((line = br.readLine()) != null) {
-                currentList.add(line.trim());
+                wordsSet.add(line.trim());
             }
+            return wordsSet;
         } catch (IOException e) {
-            System.out.println("An unexpected error has been detected with the Word List");
+            System.out.println("Failed to load the words!");
         }
-        return currentList;
+        return wordsSet;
     }
 
-    public void checkNewWords(List<String> wordsToAdd) { //To check whether the new words that the user want to add has any duplicates
-        for (String word : wordsToAdd) {
-            if (!words.contains(word)) {
+    public void saveWords() {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, false));
+            for (String word : words) {
+                bw.write(word);
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            System.out.println("Failed to add the words!");
+        }
+    }
+
+    public void addWords(Scanner scanner) {
+        System.out.println("Please enter the words that you want to add to the Word Set!");
+        System.out.println("Please note that duplicate words will not be added multiple times.");
+        System.out.println("Entering the word \"done\" will stop this process!");
+        while (true) {
+            String word = scanner.nextLine().trim();
+            if (word.equalsIgnoreCase("done")) {
+                System.out.println("Adding words process complete");
+                break;
+            }
+
+            if (word.isEmpty()) {
+                System.out.println("You have entered an empty word!");
+            } else if (words.contains(word)) {
+                System.out.println("The word " + word + " that you are trying to add already exists in the Word Set!");
+            } else {
+                System.out.println(word + " has been successfully added to the Word Set!");
                 words.add(word);
             }
         }
+        saveWords();
     }
 
-    public void addNewWordsToList() { //To add the new words that the user want to add and also checks for duplicates,doesn't add already existing words.
-        List<String> currentList = getCurrentList();
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true));
-            for (String word : words) {
-                if (!currentList.contains(word)) {
-                    bw.write(word);
-                    bw.newLine();
-                }
+    public void removeWords(Scanner scanner) {
+        System.out.println("Please enter the words that you wish to remove!");
+        System.out.println("Entering the word \"done\" will stop this process!");
+        while (true) {
+            String word = scanner.nextLine().trim();
+            if (word.equalsIgnoreCase("done")) {
+                System.out.println("Removing words process complete!");
+                break;
             }
-        } catch (IOException e) {
-            System.out.println("Unexpected error");
-        }
-    }
 
-    public void printWordList() {
-        List<String> currentList = getCurrentList();
-        for (String word : currentList) {
-            System.out.println(word);
-        }
-    }
-
-    public String getRandomWord() {
-        Random random = new Random();
-        List<String> currentList = getCurrentList();
-        return currentList.get(random.nextInt(currentList.size()));
-    }
-
-    public String[] getRandomWordLetters() {
-        String randomWord = getRandomWord();
-        String[] letters = new String[randomWord.length()];
-        for (int i = 0; i < randomWord.length(); i++) {
-            letters[i] = randomWord.substring(i, i + 1);
-        }
-        return letters;
-    }
-
-    public void addGuessedLetters(String letter) throws DuplicateItemException {
-        if (!guessedLetters.contains(letter)) {
-            guessedLetters.add(letter);
-        } else {
-            throw new DuplicateItemException("You have already guessed this letter");
-        }
-    }
-
-    public void printGuessedLetters() {
-        System.out.println("Guessed letters are:");
-        for (String letter : guessedLetters) {
-            System.out.print("  " + letter);
-        }
-    }
-
-    public List<String> getGuessedLetters() {
-        return guessedLetters;
-    }
-
-    public String getGuessedWord(String randomWord) {
-        StringBuilder sb = new StringBuilder();
-        for (char letter : randomWord.toCharArray()) {
-            if (guessedLetters.contains(String.valueOf(letter))) {
-                sb.append(letter);
+            if (word.isEmpty()) {
+                System.out.println("You have entered an empty word!");
+            } else if (!words.contains(word)) {
+                System.out.println("The word " + word + " that you are trying to remove is not in the Word Set!");
             } else {
-                sb.append(" _");
+                System.out.println(word + " has been successfully removed from the Word Set!");
+                words.remove(word);
             }
         }
-        return sb.toString();
+        saveWords();
     }
-class GameStart {
-    private GameMechanic gameMechanic = new GameMechanic();
-    private int fails;
-    private List<String> wordsToAdd;
-    public GameStart() {
 }
 
-    public void gameStart() {
-        Scanner scanner = new Scanner(System.in);
-        int gameStartScreenUserNumber;
-        List<String> wordsToAdd = new ArrayList<>();
-        System.out.println("-----------------------------------------------------------------------");
-        System.out.println("-----------------------------------------------------------------------");
-        System.out.println("-----------------Welcome to the Java Hangman Game!---------------------");
-        System.out.println("-----------------------------------------------------------------------");
-        System.out.println("-----------------------------------------------------------------------");
-        System.out.println("Press 1 if you wish to start playing! You can only guess 6 wrong letters before failing!");
-        System.out.println("Press 2 if you wish to add some words to the Word List!");
-        while(true) {
-            try {
-                gameStartScreenUserNumber = scanner.nextInt();
-                scanner.nextLine();
-                if(gameStartScreenUserNumber == 1 || gameStartScreenUserNumber == 2) {
-                    break;
-                }
-                else {
-                    System.out.println("Please enter either 1 or 2");
-                }
-            }
-            catch(InputMismatchException e) {
-                System.out.println("Please enter either 1 or 2");
-                scanner.nextLine();
-            }
-        }
-        if(gameStartScreenUserNumber == 2) {
-            int numberOfWordsToAdd = 0;
-            System.out.println("Please specify how many words you wish to add.");
-            while (true) {
-                try {
-                    numberOfWordsToAdd = scanner.nextInt();
-                    scanner.nextLine();
-                    break;
-                } catch (InputMismatchException e) {
-                    System.out.println("Please enter a number");
-                    scanner.nextLine();
-                }
-            }
-            System.out.println("You can start typing the words now! Duplicate words will be counted as one word!");
-            while (true) {
-                try {
-                    for (int i = 0; i < numberOfWordsToAdd; i++) {
-                        String word = scanner.nextLine();
-                        if (!wordsToAdd.contains(word)) {
-                            wordsToAdd.add(word);
-                        }
-                    }
-                    break;
-                } catch (InputMismatchException e) {
-                    System.out.println("Please enter a word");
-                    scanner.nextLine();
-                }
-            }
-            gameMechanic.checkNewWords(wordsToAdd);
-            gameMechanic.addNewWordsToList();
-        }
-        else if(gameStartScreenUserNumber == 1) {
 
-        }
+
+
+
+
+
+
+
 
 
 
